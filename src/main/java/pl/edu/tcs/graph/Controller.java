12679bab34e0e7e5883e9e98f775333af1fcce67
JohnBohnam <fanofledzeppelin@gmail.com>
2023-06-16 23:16:29 +0200

    Revert "tmp"
    This reverts commit 9587a9d58d5ba595505a4a93366a4281fcb6ee86.

--- src/main/java/pl/edu/tcs/graph/Controller.java
+++ src/main/java/pl/edu/tcs/graph/Controller.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
@@ -15,6 +17,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import lombok.Getter;
@@ -89,15 +92,15 @@ public class Controller {
 
         @Override
         public double getX(Vertex v) {
-            if(visualization.getGraph() instanceof GridGraph4)
-                return ((GridGraph4) visualization.getGraph()).getX(v);
+            if(visualization.getGraph() instanceof GridGraph)
+                return ((GridGraph) visualization.getGraph()).getX(v);
             return 0;
         }
 
         @Override
         public double getY(Vertex v) {
-            if(visualization.getGraph() instanceof GridGraph4)
-                return ((GridGraph4) visualization.getGraph()).getY(v);
+            if(visualization.getGraph() instanceof GridGraph)
+                return ((GridGraph) visualization.getGraph()).getY(v);
             return 0;
         }
     };
--- src/main/java/pl/edu/tcs/graph/model/GridGraph8.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.tcs.graph.model;
-
-public class GridGraph8 extends GridGraph4{
-    public GridGraph8(int width, int height) {
-        super(width, height);
-    }
-}

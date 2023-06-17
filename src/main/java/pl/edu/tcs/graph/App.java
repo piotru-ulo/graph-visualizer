d3e41616e86dfa40d9dd3d498b92a797e145e01a
JohnBohnam <fanofledzeppelin@gmail.com>
2023-06-17 02:40:24 +0200

    get a life tymek

--- src/main/java/pl/edu/tcs/graph/App.java
+++ src/main/java/pl/edu/tcs/graph/App.java
@@ -3,13 +3,16 @@ package pl.edu.tcs.graph;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class App extends Application {
+
     public void start(Stage primaryStage) throws Exception {
         URL fxmlLocation = getClass().getResource("/graph.fxml");
         FXMLLoader loader = new FXMLLoader(fxmlLocation);
+
         primaryStage = loader.load();
         primaryStage.setTitle("grapher");
         primaryStage.show();
